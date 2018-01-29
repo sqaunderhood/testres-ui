1712a09d58a0c1beeb898887366398837e56b230
Sergey Bronnikov <sergeyb@bronevichok.ru>
2018-01-30 00:08:11 +0300

    remove google analytics support

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ const dbpath = "db.sqlite"
 var (
 	httpAddr  = flag.String("http", ":8080", "HTTP service address and port")
 	staticDir = flag.String("static", "./static/", "static and templates files directory")
-	gaAccount = flag.String("gaAccount", "", "Google Analytics ID (UA-XXXXXX-Y)")
 )
 
 func main() {
