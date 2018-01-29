2c0716e34f66db55aaaa6979852ed503646eed72
Sergey Bronnikov <sergeyb@bronevichok.ru>
2018-01-30 00:08:00 +0300

    remove testanything and subunit format support

--- formats.go
+++ formats.go
@@ -8,8 +8,6 @@ import (
 	"log"
 
 	"github.com/ligurio/go-junit/parser"
-	_ "github.com/ligurio/go-subunit/parser"
-	_ "github.com/ligurio/go-tap/parser"
 )
 
 type Status int
@@ -95,64 +93,6 @@ func ReadReport(r io.Reader, name string) (*Report, error) {
 
 		log.Printf("DEBUG: REPORT %#v", report)
 		return report, nil
-
-		/*
-			} else if treport, err := tap.NewParser(r); err != nil {
-				report.Format = FmtTAP
-				log.Println("DEBUG: TAP format detected.")
-
-				ts, err := treport.Suite()
-				if err != nil {
-					log.Println("error reading TAP suites", err)
-				}
-
-				s := new(Suite)
-				s.Name = ""
-
-				for _, tl := range treport.Tests {
-					t := new(Test)
-					//t.Name = ""
-					t.Status = tl.Directive
-					t.Ok = tl.Ok
-					t.Description = tl.Description
-					t.Explanation = tl.Explanation
-					//t.StartTime = ""
-					//t.EndTime = ""
-					//t.Tags = ""
-					t.Details = tl.Yaml
-					s.Tests = append(s.Tests, t)
-				}
-
-				report.Suites = append(report.Suites, s)
-
-				log.Printf("%#v", report)
-				return report, nil
-
-			} else if sreport, err := subunit.NewParser(r); err == nil {
-				log.Println("SubUnit format detected.")
-				report.Format = FmtSubUnit
-
-				s := new(Suite)
-				s.Name = sreport.Test
-
-				for _, test := range sreport.Tests {
-					t := new(Test)
-					//t.Name = ""
-					//FIXME t.Status = test.State
-					//t.Ok = ""
-					t.Description = test.Label
-					//t.Explanation = ""
-					//t.StartTime = ""
-					//t.EndTime = ""
-					t.Tags = test.Tags
-					//t.Details = ""
-					s.Tests = append(s.Tests, t)
-				}
-
-				log.Printf("REPORT %#v", report)
-				report.Suites = append(report.Suites, s)
-				return report, nil
-		*/
 	} else {
 		log.Println("Unknown format.")
 		return nil, errors.New("Unknown format.")
